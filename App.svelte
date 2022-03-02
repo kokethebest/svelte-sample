d41249db7419bc84ecf7345378af025fef97f6f6
Jorge Rojas <koke.rojas@gmail.com>
2022-03-02 03:12:06 -0300

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Form from "./Form.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    /*text-align: center;*/
+  }
+</style>
+
+<main>
+	<h2>Películas</h2>
+  <Form />
+</main>
---
+++ Form.svelte
@@ -0,0 +1,16 @@
+<script>
+  const pelis = [
+    { name: "matrix 1", available: 2, quantity: 0 },
+    { name: "matrix 2", available: 1, quantity: 0 },
+    { name: "matrix 3", available: 5, quantity: 0 }
+  ];
+</script>
+
+{#each pelis as movie}
+<form>
+	<h3>{movie.name}</h3>
+	<button type="button" on:click={() => movie.quantity -=1} disabled={movie.quantity===0}>-</button>
+	{movie.quantity}
+	<button type="button" on:click={() => movie.quantity +=1} disabled={movie.quantity===movie.available}>+</button>
+</form>
+{/each}
